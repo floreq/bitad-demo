c9d4e320f660e51f513ed339aaa3b9ae9f90fdfe
Piotr Florczak <piotrflorczakk@gmail.com>
2020-01-03 14:56:09 +0100

    Starting set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TestComponent from "./components/TestComponent";
+import "./stylesheets/main.scss";
+
+function App() {
+  return (
+    <div>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TestComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestComponent() {
+  return <h1>ABC</h1>;
+}
+
+export default TestComponent;
