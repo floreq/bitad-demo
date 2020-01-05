705def9ace677a839847b45dc9b790ea4a48925f
Piotr Florczak <piotrflorczakk@gmail.com>
2020-01-05 11:00:10 +0100

    Dodane PolyBg

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PolyBg from "./PolyBg";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Test</h1>
+      <h2>Abc</h2>
+      <PolyBg />
+    </div>
+  );
+}
+
+export default Hero;
--- src/components/TestComponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TestComponent() {
-  return <h1>ABC</h1>;
-}
-
-export default TestComponent;
